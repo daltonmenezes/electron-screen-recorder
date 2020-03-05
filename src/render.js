03773fb4b158e1b02d2f5a28ae55db538c2222d4
Dalton Menezes <daltonmenezes@outlook.com>
2020-03-04 22:31:28 -0300

    Remove comments from the code

--- src/render.js
+++ src/render.js
@@ -4,11 +4,9 @@ const { writeFile } = require('fs');
 
 const { dialog, Menu } = remote;
 
-// Global state
-let mediaRecorder; // MediaRecorder instance to capture footage
+let mediaRecorder;
 const recordedChunks = [];
 
-// Buttons
 const videoElement = document.querySelector('video');
 
 const startBtn = document.getElementById('startBtn');
@@ -29,7 +27,6 @@ stopBtn.onclick = e => {
 const videoSelectBtn = document.getElementById('videoSelectBtn');
 videoSelectBtn.onclick = getVideoSources;
 
-// Get the available video sources
 async function getVideoSources() {
   const inputSources = await desktopCapturer.getSources({
     types: ['window', 'screen']
@@ -48,7 +45,6 @@ async function getVideoSources() {
   videoOptionsMenu.popup();
 }
 
-// Change the videoSource window to record
 async function selectSource(source) {
 
   videoSelectBtn.innerText = source.name;
@@ -63,32 +59,25 @@ async function selectSource(source) {
     }
   };
 
-  // Create a Stream
   const stream = await navigator.mediaDevices
     .getUserMedia(constraints);
 
-  // Preview the source in a video element
   videoElement.srcObject = stream;
   videoElement.play();
 
-  // Create the Media Recorder
   const options = { mimeType: 'video/webm; codecs=vp9' };
   mediaRecorder = new MediaRecorder(stream, options);
 
-  // Register Event Handlers
   mediaRecorder.ondataavailable = handleDataAvailable;
   mediaRecorder.onstop = handleStop;
 
-  // Updates the UI
 }
 
-// Captures all recorded chunks
 function handleDataAvailable(e) {
   console.log('video data available');
   recordedChunks.push(e.data);
 }
 
-// Saves the video file on stop
 async function handleStop(e) {
   const blob = new Blob(recordedChunks, {
     type: 'video/webm; codecs=vp9'
